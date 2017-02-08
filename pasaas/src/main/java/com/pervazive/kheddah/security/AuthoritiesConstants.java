aa3fed1a24c598bf191d20780a80afc35867cfeb
sshenvi <sudarshan.shenvi@gmail.com>
2017-02-08 17:43:18 +0530

    Updated Superadmin and Manager Values

--- pasaas/src/main/java/com/pervazive/kheddah/security/AuthoritiesConstants.java
+++ pasaas/src/main/java/com/pervazive/kheddah/security/AuthoritiesConstants.java
@@ -5,8 +5,9 @@ package com.pervazive.kheddah.security;
  */
 public final class AuthoritiesConstants {
 
-    public static final String ADMIN = "ROLE_ADMIN";
-
+	public static final String SUPERADMIN = "ROLE_SUPERADMIN";
+	public static final String ADMIN = "ROLE_ADMIN";
+	public static final String MANAGER = "ROLE_MANAGER";
     public static final String USER = "ROLE_USER";
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
